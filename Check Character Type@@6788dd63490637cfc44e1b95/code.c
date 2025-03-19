e8fc94613b09f5add2997a7c7398809dde48f2ad
muskaan1212 <aria18jane@gmail.com>
2025-03-19 14:31:49 +0530

    Solution to Check Character Type

---
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -0,0 +1,16 @@
+// Your code here...
+#include<stdio.h>
+int main(){
+    char n[25];
+    scanf("%s",&n);
+    if(n=aeiou){
+        printf("Vowels");
+
+    }else if(){
+        printf("Consonant");
+    }else(){
+        printf("Digital");
+    }
+    return 0;
+    
+}
